fc4d316e4a54cd240abf6280ce61aae2f205bd09
Wanjiru Catherine <catherinemunene802@gmail.com>
2020-04-14 08:57:20 +0300

    Required project files

---
+++ lib/send_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class SendData extends StatefulWidget {
+  @override
+  _SendDataState createState() => _SendDataState();
+}
+
+class _SendDataState extends State<SendData> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
